49a8c7a36a910dad5f412be9c8b29dd36b4cb466
Joost Schouwstra <63044232+jschouw1@users.noreply.github.com>
2020-06-15 15:09:42 +0200

    Started employee registation form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TablesComponent } from './tables/tables.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+
+import { MatNativeDateModule } from '@angular/material/core'
+
+// Material Imports
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablesComponent,
+    EmployeesComponent,
+    EmployeeFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    //Material Imports
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatSliderModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
